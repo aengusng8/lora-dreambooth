772a6eb3b754724ce7b3fad2f6a2d309cd7b7cf2
aengusng8 <aengus.ng8@gmail.com>
2023-07-31 10:04:13 +0700

    fix: project_dir --> logging_dir

--- training_scripts/train_lora_w_ti.py
+++ training_scripts/train_lora_w_ti.py
@@ -527,7 +527,7 @@ def parse_args(input_args=None):
         help="The token to use to push to the Model Hub.",
     )
     parser.add_argument(
-        "--logging_dir",
+        "--project_dir",
         type=str,
         default="logs",
         help=(
@@ -637,13 +637,13 @@ def unfreeze_params(params):
 
 
 def main(args):
-    logging_dir = Path(args.output_dir, args.logging_dir)
+    project_dir = Path(args.output_dir, args.project_dir)
 
     accelerator = Accelerator(
         gradient_accumulation_steps=args.gradient_accumulation_steps,
         mixed_precision=args.mixed_precision,
         log_with="tensorboard",
-        logging_dir=logging_dir,
+        project_dir=project_dir,
     )
 
     # Currently, it's not possible to do gradient accumulation when training two models with accelerate.accumulate
